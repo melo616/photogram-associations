e252ffa05524d0d87d67045c7ae1c410933f3957
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-06 16:55:24 +0000

    wrote associations through photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -31,19 +31,5 @@ class Photo < ApplicationRecord
   belongs_to(:poster, :class_name => "User", :foreign_key => "owner_id")
   has_many(:comments, :class_name => "Comment", :foreign_key => "photo_id")
   has_many(:likes, :class_name => "Like", :foreign_key => "photo_id")
-  has_many(:fans, :class_name => "User", :foreign_key => "fan_id")
-
-  def fan_list
-    my_fans = self.fans
-
-    array_of_usernames = Array.new
-
-    my_fans.each do |a_user|
-      array_of_usernames.push(a_user.username)
-    end
-
-    formatted_usernames = array_of_usernames.to_sentence
-
-    return formatted_usernames
-  end
+  has_many(:fans, :through => :likes, :source => :fan)
 end
