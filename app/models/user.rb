7e06ba5a97bf2b863c5cd46d4ef019c17792cd3b
Katherine Jarboe <110368535+melo616@users.noreply.github.com>
2023-10-06 19:32:58 +0000

    finished mandatories

--- app/models/user.rb
+++ app/models/user.rb
@@ -62,13 +62,12 @@ class User < ApplicationRecord
 # User#accepted_received_follow_requests: returns rows from the follow requests table associated to this user by the recipient_id column, where status is 'accepted'
   has_many(:liked_photos, :through => "likes", :source => "photo", :foreign_key => "photo_id")
   has_many(:commented_photos, :through => "comments", :source => "photo" )
-;
 
-  # has_many(:accepted_received_follow_requests, -> {where status: "accepted"},:class_name => "FollowRequest", :foreign_key => "recipient_id")
+  has_many(:accepted_received_follow_requests, :class_name => "FollowRequest", :foreign_key => "recipient_id")
 
-  # has_many(:followers, -> { where status: "accepted"}, :through = "accepted_received_follow_requests", :source => "user", :foreign_key => "sender")
+  # has_many(:followers, :through = "accepted_received_follow_requests", :source => "sender")
 
-  # has_many(:leaders, -> { where status: "accepted" } :through = "accepted_sent_follow_requests", :source => "user", :foreign_key => "recipient")
+  # has_many(:leaders, :through = "accepted_sent_follow_requests", :source => "recipient")
 
   has_many(:feed, :through => "leaders", :source => "own_photos")
 
